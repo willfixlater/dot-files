46b9cb65867f646a0c1aba52cab6e748e0461659
Shadizzle <mail@shayden.me>
2018-03-10 23:03:42 +0930

    Added the custom block back in, oops

--- home/user/.emacs.d/init.el
+++ home/user/.emacs.d/init.el
@@ -17,6 +17,12 @@
 (load "~/.emacs.d/mode-line.el")
 (load "~/.emacs.d/web.el")
 
+(custom-set-variables
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-safe-themes
+   (quote
+     ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))))
+
 (aligned-mode-line " "
                    '(list "%b   " mode-line-modes)
                    '(list (buffer-state-*) "λ" (buffer-state-+))
