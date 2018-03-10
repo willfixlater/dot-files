9c6d1f88627159c4a6f768cf2ff614b862e8b807
Shadizzle <mail@shayden.me>
2018-03-10 23:03:06 +0930

    Requiring js2-mode and rjsx-mode

--- home/user/.emacs.d/init.el
+++ home/user/.emacs.d/init.el
@@ -1,14 +1,15 @@
 (load "~/.emacs.d/packaging.el")
 
 (load-package-list
- '(("~/.emacs.d/elpa/packages" (seq queue spinner))
+ '(("~/.emacs.d/elpa/packages" (seq queue spinner js2-mode))
    ("~/.emacs.d/packages"      ((better-defaults "technomancy-better-defaults")
                                 (color-theme-sanityinc-tomorrow "purcell-color-theme-sanityinc-tomorrow")
                                 (paredit "campbell-paredit")
                                 (clojure-mode "clojure-emacs-clojure-mode")
                                 (cider "clojure-emacs-cider")
                                 (haskell-mode-autoloads "haskell-haskell-mode")
-                                (geiser "jaor-geiser/elisp")))))
+                                (geiser "jaor-geiser/elisp")
+                                (rjsx-mode "felipeochoa-rjsx-mode")))))
 
 (load "~/.emacs.d/clojure.el")
 (load "~/.emacs.d/paredit-hooks.el")
--- home/user/.emacs.d/web.el
+++ home/user/.emacs.d/web.el
@@ -1,2 +1,7 @@
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+
+(add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
+
 (setq js-indent-level 2)
 (setq css-indent-offset 2)
