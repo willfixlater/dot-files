ff9e5d144722a35ee5e8d394d255d99f0471014c
Shadizzle <mail@shayden.me>
2018-03-17 11:44:33 +0930

    Use rjsx for all js files, no semicolon warnings

--- home/user/.emacs.d/web.el
+++ home/user/.emacs.d/web.el
@@ -1,7 +1,6 @@
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
 
-(add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
+(setq js2-strict-missing-semi-warning nil)
 
 (setq js-indent-level 2)
 (setq css-indent-offset 2)
