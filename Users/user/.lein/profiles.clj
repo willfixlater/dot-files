2a96f91c0352de3587892416a74560dc5bde56e0
Shadizzle <mail@shayden.me>
2018-06-03 02:50:03 +1000

    Fixed lein config

--- Users/user/.lein/profiles.clj
+++ Users/user/.lein/profiles.clj
@@ -1,3 +1,5 @@
-{:user {:plugins [[lein-ancient "0.6.14"]
-                  [lein-kibit "0.1.6-beta2"]
-                  [jonase/eastwood "0.2.5"]]}}
+{:repl {:plugins [[cider/cider-nrepl "0.16.0"]]
+ :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                [com.cemerick/piggieback "0.2.2"]
+                [figwheel-sidecar "0.5.14"]]
+ :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
