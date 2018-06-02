619e584de3014343d63840d797406c3d82b0b4b1
Shadizzle <mail@shayden.me>
2018-06-03 03:37:48 +1000

    Emacs customisations for Windows

--- Users/user/.emacs.d/init.el
+++ Users/user/.emacs.d/init.el
@@ -14,11 +14,10 @@
                                 (paredit "campbell-paredit")
                                 (clojure-mode "clojure-emacs-clojure-mode")
                                 (cider "clojure-emacs-cider")
-                                (haskell-mode-autoloads "haskell-haskell-mode")
+                                (haskell-mode "haskell-haskell-mode")
                                 (geiser "jaor-geiser/elisp")
                                 (rjsx-mode "felipeochoa-rjsx-mode")))))
 
-(require 'git)
 (load "~/.emacs.d/lisp.el")
 (load "~/.emacs.d/clojure.el")
 (load "~/.emacs.d/haskell.el")
@@ -27,13 +26,17 @@
 (load "~/.emacs.d/mode-line.el")
 
 (custom-set-variables
- '(shell-pop-universal-key "C-t")
- '(shell-pop-shell-type (quote ("ansi-term" "*term*" (lambda nil (ansi-term shell-pop-term-shell)))))
- '(shell-pop-term-shell "/bin/zsh")
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
-     ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))))
+    ("8b9d07b01f2a9566969c2049faf982cab6a4b483dd43de7fd6a016bb861f7762" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(shell-pop-shell-type
+   (quote
+    ("ansi-term" "*term*"
+     (lambda nil
+       (ansi-term shell-pop-term-shell)))))
+ '(shell-pop-term-shell "powershell")
+ '(shell-pop-universal-key "C-t"))
 
 (aligned-mode-line " "
                    '(list "%b   " mode-line-modes)
@@ -52,5 +55,5 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 (set-cursor-color "#ffffff")
 (add-to-list 'default-frame-alist `(cursor-type . hbar))
-(set-face-attribute 'default nil :font "Triplicate T3c-11:weight=semi-bold:antialias=true")
-(set-face-attribute 'mode-line nil :font "Triplicate T3c-11:weight=semi-bold:antialias=true")
+(set-face-attribute 'default nil :font "Fira Code:antialias=true")
+(set-face-attribute 'mode-line nil :font "Fira Code:antialias=true")
