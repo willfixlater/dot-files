35ce5264564ab770d89c7c3a60f777c44de4dbe7
Shadizzle <mail@shayden.me>
2018-06-05 15:39:21 +1000

    Update to prettify symbols

--- Users/user/.emacs.d/prettify.el
+++ Users/user/.emacs.d/prettify.el
@@ -1,17 +1,18 @@
 (global-prettify-symbols-mode 1)
 
-(defvar my-lisp-prettify-alist
-  '(("->" . (?- (Br . Bc) ?- (Br . Bc) ?>))
+(defconst my-lisp-prettify-alist
+  '(("fn" . (?λ (Br . Bl) ?.))
+    ("*" . "×")
+    ("/" . "÷")
+    ("->" . (?- (Br . Bc) ?- (Br . Bc) ?>))
     ("->>" .  (?\s (Br . Bl) ?\s (Br . Bl) ?\s
-                             (Bl . Bl) ?- (Bc . Br) ?- (Bc . Bc) ?>
-                             (Bc . Bl) ?- (Br . Br) ?>))))
+                   (Bl . Bl) ?- (Bc . Br) ?- (Bc . Bc) ?>
+                   (Bc . Bl) ?- (Br . Br) ?>))))
 
 (eval-after-load 'clojure-mode
   '(setq clojure--prettify-symbols-alist
-         (append my-lisp-prettify-alist
-                 clojure--prettify-symbols-alist)))
+         my-lisp-prettify-alist))
 
 (eval-after-load 'lisp-mode
   '(setq lisp--prettify-symbols-alist
-         (append my-lisp-prettify-alist
-                 lisp--prettify-symbols-alist)))
+         my-lisp-prettify-alist))
