4fc720b8f0e8a7e92a2b9b9493345b4ec94c5269
shdzzl <mail@shayden.me>
2018-06-30 17:28:49 +1000

    Include user profile with kibit and ancient lein plugins

--- Users/user/.lein/profiles.clj
+++ Users/user/.lein/profiles.clj
@@ -1,5 +1,7 @@
-{:repl {:plugins [[cider/cider-nrepl "0.16.0"]]
- :dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                [com.cemerick/piggieback "0.2.2"]
-                [figwheel-sidecar "0.5.14"]]
- :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+{:user {:plugins [[lein-ancient "0.6.15"]
+                  [lein-kibit "0.1.6"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.16.0"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                       [com.cemerick/piggieback "0.2.2"]
+                       [figwheel-sidecar "0.5.14"]]
+        :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
