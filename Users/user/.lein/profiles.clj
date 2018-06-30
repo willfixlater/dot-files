6fee8fb6087024d6f005f09faf524ea4c2bd6736
shdzzl <mail@shayden.me>
2018-06-30 18:18:14 +1000

    version bump for lein profiles deps

--- Users/user/.lein/profiles.clj
+++ Users/user/.lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:plugins [[lein-ancient "0.6.15"]
                   [lein-kibit "0.1.6"]]}
  :repl {:plugins [[cider/cider-nrepl "0.16.0"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+        :dependencies [[org.clojure/tools.nrepl "0.2.13"]
                        [com.cemerick/piggieback "0.2.2"]
-                       [figwheel-sidecar "0.5.14"]]
+                       [figwheel-sidecar "0.5.16"]]
         :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
