3fd5ba62420ba5f74d64b0d39bfe354292a60d83
shdzzl <mail@shayden.me>
2018-07-02 14:25:31 +1000

    Additional changes to support github markdown and previewing

--- Users/user/.emacs.d/markdown.el
+++ Users/user/.emacs.d/markdown.el
@@ -4,3 +4,5 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+(setq markdown-command "pandoc -c file:///C:/Users/shayd/.emacs.d/styles/github-markdown.css -f gfm -t html5 --metadata=title:\"Markdown Preview\" --highlight-style pygments --standalone")
