26ccaf67ce55fa1d95fa31a15e7e3b02b3e102bf
shdzzl <mail@shayden.me>
2018-08-08 17:41:49 +1000

    Bumped cider-nrepl version

--- Users/user/.lein/profiles.clj
+++ Users/user/.lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[lein-ancient "0.6.15"]
                   [lein-kibit "0.1.6"]]}
- :repl {:plugins [[cider/cider-nrepl "0.17.0"]]
+ :repl {:plugins [[cider/cider-nrepl "0.18.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]
                        [com.cemerick/piggieback "0.2.2"]
                        [figwheel-sidecar "0.5.16"]]
