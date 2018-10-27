e7b1b8b994d3eeb0a48c21343a329ec55057a5b4
shdzzl <mail@shayden.me>
2018-10-27 14:51:43 +1100

    Clojure hooks for clj-refactor

--- Users/user/.emacs.d/clojure.el
+++ Users/user/.emacs.d/clojure.el
@@ -1,3 +1,10 @@
+(defun my-clojure-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import statements
+    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+    (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 (add-hook 'cider-mode-hook #'eldoc-mode)
 
 (setq cider-font-lock-dynamically '(macro core function var))
