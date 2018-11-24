6bc4b503e2d62ae479b5ef6314eb0dbc4a70938b
shdzzl <mail@shayden.me>
2018-11-25 00:11:19 +1100

    Configure polybar in a nix script

--- home/user/.config/nixpkgs/home.nix
+++ home/user/.config/nixpkgs/home.nix
@@ -25,48 +25,7 @@
       shadowOffsets = [ (-6) (-6) ];
     };
 
-    polybar = {
-      enable = true;
-      script = "polybar main &";
-      extraConfig = ''
-        [colors]
-        bar-background = #00000000
-        bar-foreground = #ffffff
-      '';
-      config = {
-        "bar/main" = {
-          width = "100%";
-          height = 46;
-          bottom = true;
-          fixed-center = true;
-          background = "\${colors.bar-background}";
-          foreground = "\${colors.bar-foreground}";
-          line-size = 3;
-          line-color = "#ff0000";
-          padding-left = 3;
-          padding-right = 3;
-          module-margin-left = 2;
-          module-margin-right = 2;
-          modules-right = "filesystem date";
-          cursor-click = "pointer";
-          cursor-scroll = "ns-resize";
-        };
-
-        "module/filesystem" = {
-          type = "internal/fs";
-          interval = 30;
-          mount-0 = "/";
-          label-mounted = "FS: %percentage_used%%";
-        };
-        "module/date" = {
-          type = "internal/date";
-          interval = 5;
-          date = "%B %d · %H:%M";
-          date-alt = " %Y-%m-%d";
-          label = "%date%";
-        };
-      };
-    };
+    polybar = import ./polybar.nix { inherit pkgs; };
   };  
 
   programs = {
