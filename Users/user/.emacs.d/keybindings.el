e3c184346260103d35c934e61e83bbf5a1304657
shdzzl <mail@shayden.me>
2019-01-03 11:41:27 +1100

    Re-wrote keybindings.el with new define-keys fun

--- Users/user/.emacs.d/keybindings.el
+++ Users/user/.emacs.d/keybindings.el
@@ -1,5 +1,19 @@
-(global-set-key (kbd "C-z") 'undo)
-(global-set-key (kbd "C-/") 'comment-or-uncomment-region)
-(global-set-key (kbd "C-x o") 'ace-window)
-(global-set-key (kbd "C->") 'paredit-forward-slurp-sexp)
-(global-set-key (kbd "C-<") 'paredit-backward-slurp-sexp)
+(defun define-keys (keymap &rest mappings*)
+  "Call 'define-key' on KEYMAP for all supplied MAPPINGS*.
+
+   Example: (define-keys my-keymap
+              \"C-c C-m 1\" 'my-func
+              \"C-c C-m 2\" 'my-other-func)"
+  (let ((mappings (seq-partition mappings* 2)))
+    (seq-do (lambda (mapping)
+              (seq-let [key def] mapping
+                (define-key keymap (kbd key) def)))
+            mappings)))
+
+(define-keys (current-global-map)
+  "C-z" 'undo
+  "C-/" 'comment-or-uncomment-region
+  "C-x o" 'ace-window
+  "C->" 'paredit-forward-slurp-sexp
+  "C-<" 'paredit-backward-slurp-sexp
+  "C-c C-g" 'magit-status)
