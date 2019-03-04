9e17d6274872bf424845e0638731a102be0fdc31
shdzzl <mail@shayden.me>
2019-03-04 12:24:01 +1100

    Removed .lein/profiles.clj, as I will be setting this at the project level in future

--- Users/user/.lein/profiles.clj
+++
@@ -1,9 +0,0 @@
-{:user {:plugins [[refactor-nrepl "2.4.0"]
-                  [cider/cider-nrepl "0.20.0"]
-                  [lein-ancient "0.6.15"]
-                  [lein-kibit "0.1.6"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.13"]
-                       [com.cemerick/piggieback "0.2.2"]
-                       [figwheel-sidecar "0.5.16"]]
-        :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-        :figwheel {:nrepl-middleware ["cider.nrepl/cider-middleware" "cemerick.piggieback/wrap-cljs-repl"]}}}
