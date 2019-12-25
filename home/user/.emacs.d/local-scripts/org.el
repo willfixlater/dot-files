0f4c985a37526ce11a64fd286840d3ba46936bcf
Shayden Martin <mail@shayden.me>
2019-12-25 23:57:48 +1100

    updated org mode settings

--- home/user/.emacs.d/local-scripts/org.el
+++ home/user/.emacs.d/local-scripts/org.el
@@ -1,10 +1,18 @@
 (require 'org)
 
+(defun my-org-mode-hook ()
+  (setq fill-column 100)
+  (auto-fill-mode t))
+
+(add-hook 'org-mode-hook #'my-org-mode-hook)
+
 (define-keys org-mode-map
   "M-F" 'org-metaright
   "M-B" 'org-metaleft
   "M-N" 'org-metadown
   "M-P" 'org-metaup)
+
+(setq org-confirm-babel-evaluate nil)
 
 ;; TODO: Setup babel for inline code eval
 ;; (org-babel-do-load-languages
