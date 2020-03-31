9e1aac9013a8e8480561e55f999f919af650d427
Shayden Martin <smartin@copyright.com.au>
2020-03-31 13:29:54 +1100

    Vue package set

---
+++ home/user/.emacs.d/package-sets/vue-packages.el
@@ -0,0 +1,16 @@
+;;; vue-packages.el
+
+;;; Commentary:
+;;
+;; My settings for working with vue.
+;;
+
+;;; Code:
+
+(use-package vue-mode
+  :mode "\\.vue\\'"
+  :config
+  (setq mmm-submode-decoration-level 0))
+
+(provide 'vue-packages)
+;;; vue-packages.el ends here
